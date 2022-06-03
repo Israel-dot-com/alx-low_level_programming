a44243538da887af94dd4aad6d9ac6c6d8fa2237
Israel <isltimi1000@gmail.com>
2022-06-03 04:33:05 -0700

    finally?

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,16 +2,16 @@
 
 /**
  *
- *main - Entry point
+ * main - Entry point
  *
  *
- *Return : 1;
+ * Return : 1 (Success)
  */
 
 
 int main(void)
 {
 
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-			sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19"));
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
+}
