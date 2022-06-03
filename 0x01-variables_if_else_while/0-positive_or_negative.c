bd37f46103595b6267be9874938c22f3ee0b5926
Israel <isltimi1000@gmail.com>
2022-06-03 10:39:27 -0700

    Added stdio and fixed trailing white space

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -1,20 +1,24 @@
 #include <stdlib.h>
 #include <time.h>
-/* more headers goes there */
-
-/* betty style doc for function main goes there */
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *
+ * Return: 0 Means successful
+ */
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
-	if (n>0)
+	if (n > 0)
 	{
 		printf("is positive\n");
 	}
-	else if (n==0)
+	else if (n == 0)
 	{
 		printf("is zero\n");
 	}
