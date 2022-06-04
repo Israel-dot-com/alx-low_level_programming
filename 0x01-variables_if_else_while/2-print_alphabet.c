2fdf9a33bc6005a7ec2339243cb70e3722261bf3
Israel <isltimi1000@gmail.com>
2022-06-04 03:39:17 -0700

    Used a for loop and a print f to test

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+
+/**
+ * main - Entry point
+ * Return: 0 (Success)
+ *
+ */
+
+int main()
+{
+	char az;
+
+	for(az = 'a'; az <= 'z'; ++az)
+		printf("%c", az);
+
+	return (0);
+}
