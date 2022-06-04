d761d94ac4eb4b12af3358603408f86a55b75632
Israel <isltimi1000@gmail.com>
2022-06-04 04:08:43 -0700

    Fixed indent error

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,15 +10,16 @@ int main(void)
 {
         char az;
 
-        for (az = 'a'; az <= 'z'; ++az)
+	for (az = 'a'; az <= 'z'; ++az)
 	{
 		putchar(az);
 	}
 
 	for (az = 'A'; az <= 'Z'; ++az)
-        {
-                putchar(az);
-        }
+	{
+
+		putchar(az);
+	}
 	putchar('\n');
 
 	return (0);
