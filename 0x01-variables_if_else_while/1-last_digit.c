769c2dcc5c1791bf2d82335a839c8d3ea4e3893a
Israel <isltimi1000@gmail.com>
2022-06-04 01:08:40 -0700

    Cleaned up the code and used betty Style

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -10,13 +10,14 @@
 int main(void)
 {
 	int n;
+
 	int last_digit;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	/* your code goes there */
 
-       	last_digit = n % 10;
+	last_digit = n % 10;
 
 	if (last_digit > 5)
 	{
