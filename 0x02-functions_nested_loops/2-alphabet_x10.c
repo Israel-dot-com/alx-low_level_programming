c71abd2e8a127e0863d7b0c50d3ca3809f6a8dcf
Israel <isltimi1000@gmail.com>
2022-06-07 18:25:38 -0700

    I think i got it, one shot

---
+++ 0x02-functions_nested_loops/2-alphabet_x10.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - print the lowercase alphabet x10
+ *
+ * Return: Always 0.
+ *
+ */
+
+void print_alphabet_x10(void)
+{
+
+	i = 0;
+
+	while (1 <= 10)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+	{
+		_putchar(alpha);
+	}
+	}
+	_putchar('\n');
+}
