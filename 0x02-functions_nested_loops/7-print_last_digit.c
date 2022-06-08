9e28a448313f66d6b49dab1a8c7f8691c8b30d96
Israel <isltimi1000@gmail.com>
2022-06-08 10:12:11 -0700

    Print last number with modulo

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * print_last_digit - print last digit of gven number
+ *
+ * @l: stores last number
+ *
+ * Return: Always (0).
+ */
+
+int print_last_digit(int l)
+{
+	return (l % 10);
+}
