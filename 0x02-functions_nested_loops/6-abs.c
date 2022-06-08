2cc41e283b75c5da1043eaaeaa6247f2a1e35849
Israel <isltimi1000@gmail.com>
2022-06-08 09:57:11 -0700

    Absolute number of input

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * _abs - computes the absolute value of an integer
+ *
+ * @int: holds the input to be checked
+ *
+ * Return: Always (0)
+ */
+
+int _abs(int)
+{
+	int r;
+
+	if (r < 0)
+	{
+		_putchar('-');
+		_putchar('\n');
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
