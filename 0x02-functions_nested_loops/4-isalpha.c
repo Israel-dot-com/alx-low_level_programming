39bb781ace8878e571d69cf41eddad0b8e1b44bc
Israel <isltimi1000@gmail.com>
2022-06-08 08:46:53 -0700

    is alpha task

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _isalpha - Returns values based on whether input is upper or lowercase
+ *
+ * @c: variable getting checked.
+ *
+ * Return: Always (0).
+ */
+
+int _isalpha(int c);
+{
+	if (c >= 'a' || c <= 'z' || c >= "A" || c <= 'Z')
+	{
+		return (1);
+	}
+
+	else
+	{
+		return (0);
+	}
+}
