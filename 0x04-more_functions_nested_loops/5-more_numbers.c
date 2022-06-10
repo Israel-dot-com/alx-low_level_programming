72dc27dbc4993d35cb49362c74a83736722a63d3
Israel <isltimi1000@gmail.com>
2022-06-10 12:42:33 -0700

    obligatory test commit

---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+/**
+ * more_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+	int i;
+	int j;
+
+	i = 0;
+
+	while (i <= 10)
+	{
+		j = 0;
+
+		while (j <= 14)
+		{
+			_putchar('0' + r);
+		}
+	}
+	_putchar('\n');
+}
