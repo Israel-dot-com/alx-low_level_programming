e60e490dbed3a994cc2d266efd298090bb238835
Israel <isltimi1000@gmail.com>
2022-06-10 09:53:20 -0700

    Mul task test

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * mul - multiplies two integers
+ * @a: input a
+ * @b: input b
+ * Return: 1 if c is uppercase, 0 otherwise
+ */
+
+int mul(int a, int b)
+{
+	return (int a * int b);
+}
