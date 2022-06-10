f1b60b7f73b948a1595ebaa42abba82020ce18d1
Israel <isltimi1000@gmail.com>
2022-06-10 15:27:12 -0700

    Switched to for loop cos cleaner

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -8,19 +8,18 @@ void more_numbers(void)
 	int i;
 	int j;
 
-	i = 0;
-
-	while (i <= 10)
+	for (i = 0; i <= 10; i++)
 	{
-		j = 0;
 
-		while (j <= 14)
+		for (j = 0; j <= 14; j++)
 		{
-			_putchar('0' + r);
-			++j;
+			if (j >= 10)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar('0' + j % 10);
 		}
 		_putchar('\n');
-		++i;
 	}
 
 }
