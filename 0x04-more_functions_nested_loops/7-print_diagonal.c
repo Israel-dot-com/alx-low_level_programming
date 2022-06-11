1e5290b467ccfa2fe2cd606fa74f3794175be451
Israel <isltimi1000@gmail.com>
2022-06-10 17:22:11 -0700

    diagonal task

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,31 @@
+#include "main.h"
+/**
+ * print_diagonal - check the code
+ *
+ * Return: Always 0.
+ */
+void print_diagonal(int n)
+{
+	int i;
+
+	int j;
+
+	i = 0;
+
+	while (n > 0)
+	{
+		j = i;
+		while (j > 0)
+		{
+			_putchar(' ');
+			j--;
+		}
+		_putchar('\\');
+		_putchar('\n');
+		i++;
+		n--;
+	}
+
+	if (i < 1)
+		_putchar('\n');
+}
