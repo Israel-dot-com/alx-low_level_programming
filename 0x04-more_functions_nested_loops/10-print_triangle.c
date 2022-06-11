157ae9396b3478be4d176c38a04a158892bcfe52
Israel <isltimi1000@gmail.com>
2022-06-10 18:18:53 -0700

    Trianle task

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,42 @@
+#include "main.h"
+
+/**
+ * print_triangle - print triangle
+ * @size: input
+ */
+void print_triangle(int size)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+	j = size - 1;
+	while (i < size)
+	{
+		j = size - 1 - i;
+
+		k = i + 1;
+
+		while (j > 0)
+		{
+			_putchar(' ');
+
+			j--;
+		}
+		while (k > 0)
+		{
+			_putchar('#');
+
+
+			k--;
+		}
+		_putchar('\n');
+
+
+		i++;
+	}
+	if (size <= 0)
+
+		_putchar('\n');
+}
