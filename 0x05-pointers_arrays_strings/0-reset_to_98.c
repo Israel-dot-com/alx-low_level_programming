57e4aecc80e72bbb04087e9aadfad8cc227e6dc7
Israel <isltimi1000@gmail.com>
2022-06-13 16:02:19 -0700

    First task test

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * reset_to_98 - Resets pointer to 98
+ * 
+ * @*n: Pointer to an int
+ *
+ * return: Always 0
+ */
+
+void reset_to_98(int *n)
+{
+	*n = 92;
+	return (0)
+}
