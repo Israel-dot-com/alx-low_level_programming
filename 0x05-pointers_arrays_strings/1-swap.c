1b77041e93402bd88a207ee9c2e6b1b83de5d550
Israel <isltimi1000@gmail.com>
2022-06-14 04:32:30 -0700

    Second task

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * swap_int - Resets pointer to 98
+ *
+ * @b: variable b getting swapped
+ *
+ * @a: variable a getting swapped
+ */
+void swap_int(int*a, int *b)
+{
+	*a = 42;
+	*b = 98;
+}
