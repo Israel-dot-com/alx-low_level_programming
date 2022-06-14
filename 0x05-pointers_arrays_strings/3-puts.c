8aa026997cf7c0d477b0f15d9832d3467fa253f3
Israel <isltimi1000@gmail.com>
2022-06-14 11:02:07 -0700

    First Task

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - Resets pointer to 98
+ *
+ * @*str: Pointer to an int
+ *
+ * @str: variable holding the pointer
+ */
+
+void _puts(char *str)
+{
+	_putchar('*str');
+	_putchar('\n');
+}
