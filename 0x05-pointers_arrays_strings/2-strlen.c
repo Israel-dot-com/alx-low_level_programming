263a6601dd2c477acf3ddd135fb207b6be2b301a
Israel <isltimi1000@gmail.com>
2022-06-14 05:17:54 -0700

    Test without correct return and updated main.h

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * _strl - Resets pointer to 98
+ *
+ * @s: Pointer to an char
+ *
+ * Return: (0)
+ */
+
+int _strlen(char *s);
+{
+	int strlen;
+
+	strlen = 0;
+
+	while (*s != 0)
+	{
+		strlen++;
+		s++;
+	}
+	return (0);
+}
