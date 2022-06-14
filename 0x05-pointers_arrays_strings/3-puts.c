7c03cb13d19f876524f4a9e960d855e578712ff4
Israel <isltimi1000@gmail.com>
2022-06-14 11:16:45 -0700

    Excess function declaration

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -2,16 +2,13 @@
 /**
  * _puts - Resets pointer to 98
  *
- * @i: counts string index
  *
  * @str: pointer to a char
  */
 
 void _puts(char *str)
 {
-	int i;
-
-	i = 0;
+	int i = 0;
 
 	while (str[i] != '\0')
 	{
