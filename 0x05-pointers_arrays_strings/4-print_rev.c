5a6e18b29b9ae1701b73b034625677a55fc6bebd
Israel <isltimi1000@gmail.com>
2022-06-14 11:25:51 -0700

    Reverse string test

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,26 @@
+#include "main.h"
+/**
+ * print_rev - Reverse a string
+ *
+ * @s: Pointer to an int
+ *
+ */
+
+void print_rev(char *s)
+{
+	int i = 0;
+
+	while (*s = '\0')
+	{
+		++s;
+		++i;
+	}
+
+	while (i > 0)
+	{
+		--s;
+		_putchar('*s');
+		--count;
+	}
+
+}
