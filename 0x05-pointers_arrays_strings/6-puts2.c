cf487001b7ece4d585e6a734fd4e5ebbfd6ad03d
Israel <isltimi1000@gmail.com>
2022-06-14 13:15:54 -0700

    TAsk 6

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * puts2 - prints every even index of character
+ *
+ *
+ * @str: variable holding the pointer
+ */
+void puts2(char *str)
+{
+	int i;
+
+	i = 0;
+
+	while (str[i] != '\0')
+	{
+		if (i % 2)
+		{
+			_putchar(str[i]);
+		}
+		i++
+	}
+}
+
