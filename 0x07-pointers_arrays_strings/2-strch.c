2b0f941cd093c69e451db9f81aa7885d913d4156
Israel <isltimi1000@gmail.com>
2022-06-20 14:00:33 -0700

    Push all file and rough starter for mandatory tasks

---
+++ 0x07-pointers_arrays_strings/2-strch.c
@@ -0,0 +1,12 @@
+#include "main.h"
+/**
+ * xxx-
+ * @s:
+ * @c:
+ *
+ * Return: Always(0)
+ */
+type prototype()
+{
+	return ();
+}
---
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * xxx-
+ * @s:
+ * @c:
+ *
+ * Return: Always(0)
+ */
+type prototype()
+{
+        return ();
+}
+
