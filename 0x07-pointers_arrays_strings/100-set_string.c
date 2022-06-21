34fb761de03c84a4143f67172683a324f7f3a37e
Israel <isltimi1000@gmail.com>
2022-06-21 15:48:50 -0700

    A advanced task

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * set_string - sets value pointer to char
+ *
+ * @s: Double pointer
+ *
+ * @to: Pointer
+ *
+ * Return: Void
+ */
+
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
