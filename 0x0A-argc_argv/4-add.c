e35fcf7c837910ba04a205d6e5bb45d7b7be65cd
Israel <isltimi1000@gmail.com>
2022-06-24 19:37:55 -0700

    wqPlaceholder

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -13,29 +13,6 @@
 
 int main(int argc, char *argv[])
 {
-	int main(int argc, char *argv[])
-{
-	int total, i;
-	char *p;
-	int num;
-
-	total = 0;
-	if (argc > 1)
-	{
-		for (i = 1; argv[i]; i++)
-		{
-			num = strtol(argv[i], &p, 10);
-			if (!*p)
-				total += num;
-			else
-			{
-				printf("Error\n");
-				return (1);
-			}
-		}
-	}
-	printf("%d\n", total);
-	return (0);
-
-	return (0);
+	void(argc);
+	void(argv);
 }
