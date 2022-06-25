825a24c9d07bc683d624984ddaf609f5850c8e04
Israel <isltimi1000@gmail.com>
2022-06-24 18:52:41 -0700

    Fix Typo in directory name

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @argc: Argument Counter
+ *
+ * @argv: Argument Vector
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 0)
+	{
+		printf("%s\n", argv[0]);
+	}
+
+	return (0);
+}
