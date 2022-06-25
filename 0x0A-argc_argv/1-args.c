f35bfcf85bc294234d6670bcf99f5267673aaa43
Israel <isltimi1000@gmail.com>
2022-06-24 18:59:32 -0700

    Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * @argc: Argument Counter
+ *
+ * @argv: Argument Vector
+ *
+ * Return: Always (0)
+ */
+
+int main(int argc, char *argv[])
+{
+	if (argc >= 1)
+		printf("%d\n", argc);
+
+	return (0);
+}
