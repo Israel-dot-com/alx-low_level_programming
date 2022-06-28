d420a868efdf4ddf9b9ae5160a26289b311465f9
Israel <isltimi1000@gmail.com>
2022-06-28 16:16:14 -0700

    Final mandatory tasks

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,12 +1,43 @@
 #include "main.h"
 #include <stdlib.h>
 /**
- * _strdup - Duplicate a string using malloc
- * @str: string to duplicate
- * Return: Pointer to a the new duped string
+ * str_concat - Concatenates strings
+ *
+ * @s1: String 1
+ *
+ * @s2: String 2
+ *
+ * Return: Always (0)
  */
 
-char *_strdup(char *str)
+char *str_concat(char *s1, char *s2)
 {
+	unsigned int s1_length = 0;
+	unsigned int s2_length = 0;
+	char *alloc_mem;
+	unsigned int i, j;
 
+	if (s1 == NULL)
+		s1 = "";
+
+	if (s2 == NULL)
+		s2 = "";
+
+	while (*(s1 + s1_length) != '\0')
+		s1_length++;
+
+	while (*(s2 + s2_length) != '\0')
+		s2_length++;
+
+	alloc_mem = malloc(1 + (s1_length * sizeof(*s1)) + (s2_length * sizeof(*s2)));
+	if (alloc_mem == NULL)
+		return ('\0');
+
+	for (i = 0; i < s1_length; i++)
+		alloc_mem[i] = *(s1 + i);
+
+	for (j = 0; j < s2_length; j++, i++)
+		alloc_mem[i] = *(s2 + j);
+
+	return (alloc_mem);
 }
