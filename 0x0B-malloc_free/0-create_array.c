7f804f670d8871bed8208fac8004a735d9d845b4
Israel <isltimi1000@gmail.com>
2022-06-28 06:47:57 -0700

    Add prototypes to main

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * @create_array - This function creates an array of char and initializes it with a particular char
+ *
+ * @size: This paramenter holds the size of the array
+ * @c: This is the second parameter.
+ * Return: Always (0).
+ */
+
+int char *create_array(unsigned int size, char c)
+{
+	int n;
+	n = 0;
+
+
+}
