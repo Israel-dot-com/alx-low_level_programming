b7f74ebcd0568275be5c3a398f49a91f31421620
Israel <isltimi1000@gmail.com>
2022-06-28 15:35:03 -0700

    Add task 4

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * _strdup - Duplicate a string using malloc
+ * @str: string to duplicate
+ * Return: Pointer to a the new duped string
+ */
+
+char *_strdup(char *str)
+{
+
+}
