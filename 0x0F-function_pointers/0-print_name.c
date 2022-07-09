6eac41be5f80d3df71dff716d861adfa078a1a13
Israel <isltimi1000@gmail.com>
2022-07-08 17:03:18 -0700

    Readded tasks

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -1,8 +1,8 @@
 #include "function_pointers.h"
-
+#include <stdio.h>
 /**
- * print_name - Prints a name.
- * @name: The name to be printed.
+ * print_name - Prints a name
+ * @name: The name that is printed
  * @f: A pointer to a function that prints a name.
  */
 void print_name(char *name, void (*f)(char *))
