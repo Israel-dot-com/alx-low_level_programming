ab9a98937687c4de65ac2379a8460cf05621ecfc
Israel <isltimi1000@gmail.com>
2022-07-08 17:09:18 -0700

    Task 0+1

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,22 @@
+#include "function_pointers.h"
+
+/**
+ * array_iterator - Executes a function given as a param
+ * on each element of array
+ * @array: The array
+ * @size: The size of given array
+ * @action: A pointer to the function to be executed
+ */
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	if (array == NULL || action == NULL)
+		return;
+
+
+	while (size-- > 0)
+
+	{
+		action(*array);
+		array++;
+	}
+}
