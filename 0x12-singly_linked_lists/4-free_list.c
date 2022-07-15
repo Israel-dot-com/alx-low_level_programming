467f35f619a9af88621ccfc07521395ee19b604f
Israel <isltimi1000@gmail.com>
2022-07-15 16:54:36 -0700

    Fix free list task

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,14 +1,20 @@
 #include "lists.h"
 /**
- * free_list - frees a linked list
- * @head: list to be freed
+ * free_list - free a `list_t` list
+ * @head: head of linked list
  */
 void free_list(list_t *head)
 {
-	while (head != NULL)
+	list_t *current;
+	list_t *nxt;
+
+	current = head;
+
+	while (current != NULL)
 	{
-		free(head->str);
-		free(head);
-		head = head->next;
+		nxt = current->next;
+		free(current->str);
+		free(current);
+		current = nxt;
 	}
 }
